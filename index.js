ec02e33ad85b35061875872e170e359ac419803a
+ <+>
2024-07-14 13:41:06 -0700

    -m tried to add functions for employee records

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Your code here
+function createEmployeeRecord(firstName, familyName, title, payPerHour) {
+    return {
+      firstName,
+      familyName,
+      title,
+      payPerHour,
+      timeInEvents: [],
+      timeOutEvents: [],
+    };
+  }
+  
+  
+  function createEmployeeRecords(employeeData) {
+    return employeeData.map(employee => createEmployeeRecord(...employee));
+  }
+  
+  function createTimeInEvent(employee, timeStamp) {
+    const [date, time] = timeStamp.split(' ');
+    employee.timeInEvents.push({ type: 'TimeIn', date, hour: parseInt(time) });
+    return employee;
+  }
+  
+  function createTimeOutEvent(employee, timeStamp) {
+    const [date, time] = timeStamp.split(' ');
+    employee.timeOutEvents.push({ type: 'TimeOut', date, hour: parseInt(time) });
+    return employee;
+  }
+  
+  function hoursWorkedOnDate(employee, date) {
+    const timeIn = employee.timeInEvents.find(event => event.date === date);
+    const timeOut = employee.timeOutEvents.find(event => event.date === date);
+    if (!timeIn || !timeOut) return 0;
+    return (timeOut.hour - timeIn.hour) / 100;
+  }
+  
+  function wagesEarnedOnDate(employee, date) {
+    return hoursWorkedOnDate(employee, date) * employee.payPerHour;
+  }
+  
+  function allWagesFor(employee) {
+    const uniqueDates = new Set(employee.timeInEvents.map(event => event.date));
+    return Array.from(uniqueDates).reduce((total, date) => total + wagesEarnedOnDate(employee, date), 0);
+  }
+  
+  function calculatePayroll(employees) {
+    return employees.reduce((total, employee) => total + allWagesFor(employee), 0);
+  }
+  
